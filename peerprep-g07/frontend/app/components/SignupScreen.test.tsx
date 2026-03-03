dadb662d0aee7cdc333fd80b2f938456081c7789
Paul <paulcheng46@gmail.com>
2026-03-03 22:14:05 +0800

    Remove unnecessary peerprep folder

--- peerprep-g07/frontend/app/components/SignupScreen.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import SignupScreen from './SignupScreen';
-
-test('renders SignupScreen component', () => {
-    render(<SignupScreen />);
-    const linkElement = screen.getByText(/sign up/i);
-    expect(linkElement).toBeInTheDocument();
-});
